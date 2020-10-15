33f0563752876a305b97b47a8541e9077c52dfa8
Priya Sharma <54985329+priyash243@users.noreply.github.com>
2020-10-15 17:50:38 +0530

    Update pil.py

--- pil.py
+++ pil.py
@@ -1,6 +1,6 @@
 from Pil import Image,ImageChops
 img1=Image.open('img1.jpg')
-img2=Image.open('ima2.jpg')
+img2=Image.open('img2.jpg')
 diff=ImageChops.difference(img1,img2)
 print(diff.getbox()) #shows difference in no. of  pixels
 if diff.getbox():
